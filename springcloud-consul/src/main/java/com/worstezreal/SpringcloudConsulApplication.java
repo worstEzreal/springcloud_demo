baecc3384559baf42b0dfaaf9ec1d28f0940f890
zengxzh <zengxzh@yonyou.com>
2019-04-10 09:25:44 +0800

    1.添加阿波罗 2.模块名字修改

--- springcloud-consul/src/main/java/com/worstezreal/SpringcloudConsulApplication.java
+++ springcloud-consul/src/main/java/com/worstezreal/SpringcloudConsulApplication.java
@@ -1,6 +1,10 @@
 package com.worstezreal;
 
+import com.ctrip.framework.apollo.Config;
+import com.ctrip.framework.apollo.spring.annotation.ApolloConfig;
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import com.worstezreal.entity.Result;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableApolloConfig
 public class SpringcloudConsulApplication {
 
     public static void main(String[] args) {
@@ -18,9 +23,21 @@ public class SpringcloudConsulApplication {
     @RestController
     static class TestController {
 
+        @ApolloConfig
+        private Config config;
+
+        @Value("${rest.connect.timeout}")
+        private String timeout;
+
         @RequestMapping("/hello")
         public Result hello() {
             return new Result(0, "hello world");
+        }
+
+        @RequestMapping("/config")
+        public Result config() {
+            return new Result(0, "timeout is : " + timeout);
+//            return new Result(0, "timeout is : " + config.getIntProperty("rest.connect.timeout", 0));
         }
 
     }
